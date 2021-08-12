9aaa20c435561c07cc12dad5684e917040240474
Hyoyoung Park <gydudwls@gmail.com>
2021-08-13 01:43:01 +0900

    Calla

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,63 @@
 import Head from "next/head";
-import Image from "next/image";
 import { GetServerSideProps } from "next";
 import { getSongInfo, ISongInfo } from "./api/song";
+import {
+  Container,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  VStack,
+  Text,
+  Img,
+  Heading,
+} from "@chakra-ui/react";
 
 export default function Home({ song }: { song: ISongInfo }) {
   return (
     <>
       <Head>
         <title>Calla</title>
-        <meta name="description" content="Recomend Random Classical Music" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Recommend Random Classical Music" />
+        <link type="image/svg+xml" rel="icon" href="/Calla.svg" />
       </Head>
 
-      <main style={{ height: "100%", display: "grid", placeContent: "center" }}>
-        <video controls={true} autoPlay={true} loop={true}>
-          <source src={song.file}></source>
-        </video>
-      </main>
+      <Container as={"main"}>
+        <VStack marginTop={{ base: "28vh", md: "10vh" }}>
+          <Img src={"/Calla.svg"} w={{ base: 16, md: 32 }}></Img>
+          <Heading>Calla</Heading>
+          <Text>랜덤으로 클래식 음악을 추천해줍니다.</Text>
+          <audio controls={true} autoPlay={true} loop={true}>
+            <source src={song.file}></source>
+          </audio>
+          <Table display={{ base: "none", md: "table" }} variant="simple">
+            <TableCaption>곡 정보</TableCaption>
+            <Thead>
+              <Tr>
+                {Object.keys(song.info).map((keyName, i) => (
+                  <Th key={`key-${i}}`}>{keyName}</Th>
+                ))}
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                {Object.keys(song.info).map((keyName, i) => (
+                  <Td key={`info-${i}`}>{song.info[keyName] as any}</Td>
+                ))}
+              </Tr>
+            </Tbody>
+          </Table>
+        </VStack>
+      </Container>
     </>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const song = await getSongInfo();
-
+  console.log(song);
   return { props: { song } };
 };
